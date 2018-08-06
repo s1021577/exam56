3882d131fded44ab4e2cb495d41ff14b7a0f6505
s1021577 <s1021577@stm.sfhs.ntpc.edu.tw>
2018-08-06 15:23:19 +0800

    抽換樣本

--- routes/web.php
+++ routes/web.php
@@ -9,10 +9,21 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'tad4';
+    $say  = '嗨！';
+    return view('welcome', compact('name', 'say'));
+
+    //return view('welcome', ['name' => 'tad3', 'say' => '嗨！']);
+
+    // $data = ['name' => 'tad2', 'say' => '嗨！'];
+    // return view('welcome', $data);
+
+    // return view('welcome')
+    //     ->with('name', 'tad1')
+    //     ->with('say', '嗨！');
 });
 
 Auth::routes();
