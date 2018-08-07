43bf22131271f2de3371e4cb1616cf59326543bc
s1021577 <s1021577@stm.sfhs.ntpc.edu.tw>
2018-08-07 16:29:44 +0800

    完成列表0807end

--- resources/views/exam/index.blade.php
+++ resources/views/exam/index.blade.php
@@ -4,13 +4,19 @@
 @section('content')
     <h1>隨機題庫系統</h1>
 
-    @forelse($exams as $exam)
-        <li>{{ $exam->title }}</li>
-    @empty
-        <div class="alert alert-danger">
-            尚無任何測驗
-        </div>
-    @endforelse
+    <div class="list-group">      
+
+        @forelse($exams as $exam)
+            
+            <a href="exam/{{$exam->id}}" class="list-group-item list-group-item-action">
+                {{$exam->updated_at->format('Y年m月d日')}}{{ $exam->title }}</a>
+        @empty
+            <div class="alert alert-danger">
+                尚無任何測驗
+            </div>
+        @endforelse
+        
+    </div>
 
 @endsection
 
